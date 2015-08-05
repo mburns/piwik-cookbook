637a55b8137efd6e4835dee5b79c11b892e94309
Michael Burns <michael.burns@rackspace.com>
2015-08-05 03:23:56 -0700

    Fix syntax

--- recipes/master.rb
+++ recipes/master.rb
@@ -2,7 +2,7 @@ include_recipe 'piwik::default'
 
 cron 'hourly_piwik_archive' do
   minute '12'
-  user node[:nginx][:user]
-  mailto node[:piwik][:config][:superuser][:email] || 'root'
-  command "#{node[:piwik][:install_path]}/piwik/misc/cron/archive.sh > /dev/null"
+  user node['nginx']['user']
+  mailto node['piwik']['config']['superuser']['email'] || 'root'
+  command "#{node['piwik']['install_path']}/piwik/misc/cron/archive.sh > /dev/null"
 end
