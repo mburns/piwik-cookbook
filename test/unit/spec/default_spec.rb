a3b4e2ce594b7c9c15ea5e9a74a851935ecd6104
Michael Burns <michael.burns@rackspace.com>
2015-08-15 15:19:55 -0700

    unit tests

--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -4,5 +4,40 @@ require_relative 'spec_helper'
 describe 'piwik::default' do
   before { stub_resources }
 
-  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+  let(:chef_run) { ChefSpec::SoloRunner(step_into: ['iptables', 'logrotate', 'runit']).converge(described_recipe) }
+
+  it 'enables piwik virtualhost' do
+    expect(chef_run).to run_bash('enable piwik site')
+  end
+
+  it 'includes the `logrotate` recipe' do
+    expect(chef_run).to include_recipe('logrotate::default')
+  end
+
+  it 'configures nginx log rotation' do
+    expect(chef_run).to logrotate_app_create('nginx')
+  end
+
+  it 'includes the `runit` recipe' do
+    expect(chef_run).to include_recipe('runit::default')
+  end
+
+  it 'configures piwik runit service' do
+    expect(chef_run).to runit_service_create('piwik')
+  end
+
+  it 'includes the `iptables` recipe' do
+    expect(chef_run).to include_recipe('iptables::default')
+  end
+
+  it 'adds firewall rule for nginx' do
+    expect(chef_run).to iptables_rule_rule('iptables_http')
+  end
+  it 'creates a remote_file of piwik tarball' do
+    expect(chef_run).to create_remote_file_if_missing('/var/chef/cache/piwik-1.5.1.tar.gz')
+  end
+
+  it 'runs install_piwik script' do
+    expect(chef_run).to run_bash('install_piwik')
+  end
 end
--- test/unit/spec/slave_spec.rb
+++ test/unit/spec/slave_spec.rb
@@ -5,4 +5,9 @@ describe 'piwik::slave' do
   before { stub_resources }
 
   let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+  it 'restarts php-fastcgi' do
+    resource = chef_run.template('/var/www/piwik/config/config.ini.php')
+    expect(resource).to notify('service[php-fastcgi]').to(:restart).immediately
+  end
 end
