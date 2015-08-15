901e90023ddb51c892cd8a9263f53f800071e478
Michael Burns <michael.burns@rackspace.com>
2015-08-15 15:01:36 -0700

    more integration tests

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,14 +8,14 @@ default['piwik']['php_fcgi_bind_path'] = '/tmp/.php-fcgi-socket'
 default['piwik']['php_fcgi_pass'] = 'unix:/tmp/.php-fcgi-socket'
 
 # Params used in slave recipe only:
-default['piwik']['config']['superuser']['login'] = 'override_attribute in role!'
-default['piwik']['config']['superuser']['password'] = 'override_attribute in role!'
-default['piwik']['config']['superuser']['email'] = 'override_attribute in role!'
-default['piwik']['config']['superuser']['salt'] = 'override_attribute in role!'
+default['piwik']['config']['superuser']['login'] = '' # override_attribute in role!
+default['piwik']['config']['superuser']['password'] = '' # override_attribute in role!
+default['piwik']['config']['superuser']['email'] = '' # override_attribute in role!
+default['piwik']['config']['superuser']['salt'] = '' # override_attribute in role!
 default['piwik']['config']['database']['host'] = 'localhost'
 default['piwik']['config']['database']['port'] = '3306'
 default['piwik']['config']['database']['username'] = 'piwik'
-default['piwik']['config']['database']['password'] = 'override_attribute in role!'
+default['piwik']['config']['database']['password'] = '' # override_attribute in role!
 default['piwik']['config']['database']['dbname'] = 'piwik'
 default['piwik']['config']['database']['adapter'] = 'PDO_MYSQL'
 default['piwik']['config']['database']['tables_prefix'] = 'piwik_'
